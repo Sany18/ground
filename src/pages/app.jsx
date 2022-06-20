d60914184c2ae1f8675b2e01cdbe9b2e03c796fc
Alex <o.tantsiura@spd-ukraine.com>
2022-06-20 21:37:29 +0300

    changed root

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,5 +1,4 @@
-// url: /
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 import Header from 'pages/_header';
 import Footer from 'pages/_footer';
@@ -11,17 +10,17 @@ import './app.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
         <div className="wrapper">
           <Routes>
             <Route path="/" element={<TitlePage />} />
             <Route path="/product/:id" element={<ProductIdPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
+            {/* <Route path="/" element={<Navigate to="/" />} /> */}
           </Routes>
         </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
